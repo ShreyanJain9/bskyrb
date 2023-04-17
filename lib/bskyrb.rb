d5fa2ca2bac5cb751f0aab3e0d14a57d72145504
ShreyanJain9 <shreyan.jain.9@outlook.com>
2023-04-16 23:47:10 -0700

    Version 0.1.0 Login and put your DID

---
+++ lib/bskyrb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bskyrb/version"
+
+module Bskyrb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bskyrb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Bskyrb
+  VERSION = "0.1.0"
+end
