feeac67dbbc11da8141d9bda6b383d210eefd474
ShreyanJain9 <shreyan.jain.9@outlook.com>
2023-04-28 20:01:54 -0700

    switched to require_relative for easier testing

--- lib/bskyrb.rb
+++ lib/bskyrb.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-require "bskyrb/session"
-require "bskyrb/records"
-require "bskyrb/generated_classes"
+require_relative "bskyrb/session"
+require_relative "bskyrb/records"
+require_relative "bskyrb/generated_classes"
